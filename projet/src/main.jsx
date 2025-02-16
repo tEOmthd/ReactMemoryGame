fcc1033c035d37594cb1b6e07f9dc4233c9a9725
Teo Mathiaud <teo.mathiaud@etu.univ-amu.fr>
2025-02-16 14:45:09 +0100

    Fix import

--- projet/src/main.jsx
+++ projet/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style/index.css'
-import App from x"./App.jsx";
+import App from "./App.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
